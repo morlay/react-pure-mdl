153bfd5b84ca273b20632c481bf87ff491a144b7
Morlay <morlay@qq.com>
2015-12-28 20:57:29 +0800

    publish folder should only js

--- gulpfile.babel.js/config/test/index.js
+++ gulpfile.babel.js/config/test/index.js
@@ -1,13 +1,15 @@
 import { argv } from 'yargs';
 
 export default (conf) => {
+  process.env.BABEL_ENV = 'node';
+
   return {
     src: [
       `${conf.src}/**/__tests__/*.spec.js`
     ],
     options: {
       R: 'dot',
-      compilers: `.:${conf.helpers}/require-hooks.js`,
+      compilers: '.:babel-register',
       istanbul: !!argv.coverage ? {
         bin: require.resolve('./bin/babel-istanbul')
       } : false
