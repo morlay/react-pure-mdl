4889e1e8ea5acb12a8cbc704eb786a5d0baf95a0
Morlay <morlay@qq.com>
2016-01-10 17:07:15 +0800

    use enzyme as the test units

--- src/icons/__tests__/Icon.spec.js
+++ src/icons/__tests__/Icon.spec.js
@@ -1,18 +1,17 @@
 import { expect } from 'chai';
 
 import React from 'react';
-import legitTest from 'legit-tests';
+import { shallow } from 'enzyme';
+
 import Icon from '../Icon';
 
 describe(__filename, () => {
   context('when render with prop name', () => {
     it('should have className with "material-icons"', () => {
       const iconName = 'test';
-      legitTest(<Icon name={iconName}/>, { shallow: true })
-        .test(({ instance }) => {
-          expect(instance.props.className).to.eql('material-icons');
-          expect(instance.props.children).to.eql(iconName);
-        });
+      const wrapper = shallow(<Icon name={iconName}/>);
+      expect(wrapper.props().className).to.eql('material-icons');
+      expect(wrapper.props().children).to.eql(iconName);
     });
   });
 });
